e719cd43a24f607d1ea9ee17d57f428297e0c066
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2019-01-29 11:49:16 +0100

    Dodano funkcję read_key()

--- new_conio.h
+++ new_conio.h
@@ -2,8 +2,11 @@
 #define __NEW_CONIO_H_
 
 #include <Windows.h>
+#include <assert.h>
 
 static HANDLE houtput = NULL;
+static HANDLE hinput = NULL;
+
 static int current_foreground = 0;
 static int current_background = 0;
 
@@ -86,6 +89,7 @@ static void set_screen_size(int width, int height);
 static void sleep(int time_ms);
 static int get_time(void);
 static void gotoxy(int x, int y);
+static int read_key(DWORD milliseconds);
 
 
 static void set_screen_size(int width, int height)
@@ -94,35 +98,43 @@ static void set_screen_size(int width, int height)
 	SMALL_RECT r;
 	COORD      c;
 	houtput = GetStdHandle(STD_OUTPUT_HANDLE);
+	hinput = GetStdHandle(STD_INPUT_HANDLE);
 
 	set_background(BLACK);
 	set_foreground(WHITE);
 
-	if (!GetConsoleScreenBufferInfo(houtput, &csbi))
-		exit(1);
-
-	c.X = width;
-	c.Y = height;
-	BOOL a = SetConsoleScreenBufferSize(houtput, c);
+	BOOL ret = GetConsoleScreenBufferInfo(houtput, &csbi);
+	assert(ret && "GetConsoleScreenBufferInfo");
 
 	r.Left = r.Top = 0;
 	r.Right = width - 1;
 	r.Bottom = height - 1;
-	BOOL b = SetConsoleWindowInfo(houtput, TRUE, &r);
+	ret = SetConsoleWindowInfo(houtput, TRUE, &r);
+	assert(ret && "SetConsoleWindowInfo");
+
+	c.X = width;
+	c.Y = height;
+	ret = SetConsoleScreenBufferSize(houtput, c);
+	assert(ret && "SetConsoleScreenBufferSize");
+
 }
 
 static void gotoxy(int x, int y)
 {
 	COORD pos = { x, y };
-	BOOL b = SetConsoleCursorPosition(houtput, pos);
+	BOOL ret = SetConsoleCursorPosition(houtput, pos);
+	assert(ret && "SetConsoleCursorPosition");
 }
 
 static void set_cursor_visibility(int visible)
 {
 	CONSOLE_CURSOR_INFO cci;
-	GetConsoleCursorInfo(houtput, &cci);
+	BOOL ret = GetConsoleCursorInfo(houtput, &cci);
+	assert(ret && "GetConsoleCursorInfo");
+	
 	cci.bVisible = visible;
-	SetConsoleCursorInfo(houtput, &cci);
+	ret = SetConsoleCursorInfo(houtput, &cci);
+	assert(ret && "SetConsoleCursorInfo");
 }
 
 static void show_cursor(void)
@@ -138,7 +150,8 @@ static void hide_cursor(void)
 static void set_background(int back_color)
 {
 	current_background = back_color & 0x0F;
-	SetConsoleTextAttribute(houtput, (current_background << 4) | current_foreground);
+	BOOL ret = SetConsoleTextAttribute(houtput, (current_background << 4) | current_foreground);
+	assert(ret && "SetConsoleTextAttribute");
 }
 
 static void set_foreground(int fore_color)
@@ -156,6 +169,39 @@ static void sleep(int time_ms)
 {
 	Sleep(time_ms);
 }
+
+
+
+static int read_key(DWORD milliseconds)
+{
+	ULONGLONG start_time = GetTickCount64();
+
+	INPUT_RECORD record;
+	DWORD numRead;
+	while (TRUE)
+	{
+		// czy minął czas?
+		ULONGLONG now = GetTickCount64();
+		if (now - start_time > milliseconds)
+			return -1; // czas się skończył
+
+		DWORD result = WaitForSingleObject(hinput, 10);
+		if (result == WAIT_TIMEOUT)
+			continue;
+
+		BOOL ret = ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &record, 1, &numRead);
+		assert(ret && "ReadConsoleInput");
+
+		if (record.EventType != KEY_EVENT)
+			continue;
+
+		// stałe klawiszy dostępne pod VK_?????
+		if (record.Event.KeyEvent.bKeyDown)
+			return record.Event.KeyEvent.wVirtualKeyCode;
+	}
+
+}
+
 
 #endif // __NEW_CONIO_H_
 
